d57e8b2a82006911758ca5d2653ba63cac64fa22
Dushan Anuradha <ohda456@gmail.com>
2024-04-24 07:42:28 +1000

    modf_test_spec

--- cypress/e2e/npm-tests/homepage.cy.ts
+++ cypress/e2e/npm-tests/homepage.cy.ts
@@ -1,11 +1,16 @@
 /// <reference types="cypress" />
 
-describe("Home page", ()=>{
-    before("Navigate to npm home page", ()=>{
-        cy.visit(Cypress.env("BASE_URL"), { failOnStatusCode: false });
-    })
+describe("Home page", () => {
+  before("Navigate to npm home page", () => {
+    cy.visit(Cypress.env("BASE_URL"), { failOnStatusCode: false });
+  });
 
-    it("Verify correct address", ()=>{
-        cy.get('#nav-docs-link').click()
-    })
-})
+  it("Verify correct address", () => {
+    cy.get("#nav-docs-link").click();
+    cy.go("back").then(() => {
+      cy.get('input[type="search"]').clear().type("cypress").click();
+      cy.get('ul[aria-label="Search results"]', { timeout: 10000 }).contains('cypress-split').click()
+      cy.url().should('have.string','https://www.npmjs.com/package/cypress-spli')
+    });
+  });
+});
