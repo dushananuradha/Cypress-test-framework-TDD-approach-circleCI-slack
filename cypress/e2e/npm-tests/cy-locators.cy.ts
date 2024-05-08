c80935b19d21c3bb84dfd47103a09dccbba265ae
Dushan Anuradha <ohda456@gmail.com>
2024-05-09 06:43:27 +1000

    removed_branchFilter

--- cypress/e2e/npm-tests/cy-locators.cy.ts
+++ cypress/e2e/npm-tests/cy-locators.cy.ts
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
+let data:any;
+
+beforeEach("load fixture data", function() {
+    cy.fixture('fillOutForms.json').then((fixtureData) => {
+      this.data = fixtureData;
+    });
+  });
 describe("Cypress element locators", () => {
     beforeEach("Navigate to npm home page", () => {
         cy.visit(Cypress.env("UI_BASE_URL"), { failOnStatusCode: false });
+        
     });
 
     it("Verify home page URL", () => {
@@ -15,5 +23,13 @@ describe("Cypress element locators", () => {
             cy.get('li > a').eq(1).should('have.attr', 'href', '../fake-landing-page');
         })
     })
+
+    it("dynamic testing using fixtures", function() {
+        cy.get('li > a').eq(3).should('have.attr', 'href', '../Fill out forms').click();
+        cy.url().should('equal', Cypress.env("UI_BASE_URL"))
+        cy.log(this.data.company);
+        cy.log(this.data.contact_name);
+        cy.log(this.data.contact_email);
+      });
 
 });
