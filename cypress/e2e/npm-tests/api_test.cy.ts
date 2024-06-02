97ae99704656b3bfb987a7a5c8ba9c14289b536e
Dushan Anuradha <ohda456@gmail.com>
2024-06-02 16:47:06 +1000

    added_pgObj_API

---
+++ cypress/e2e/npm-tests/api_test.cy.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+import {userAPI} from "../../pageObjects/API/users";
+
+let user = new userAPI();
+
+describe("User APIs", ()=>{
+
+    before("Navigate to npm home page", () => {
+        cy.visit(Cypress.env("BASE_URL"), { failOnStatusCode: false });
+      });
+
+    it("verify get users", ()=>{
+        user.getUsers();
+        user.createUser();
+    });
+});
---
+++ cypress/pageObjects/API/users.ts
@@ -0,0 +1,28 @@
+import { APIRequest } from "../API/globalAPIS";
+import { userJSON } from "../API/user_data";
+
+const user_payload = userJSON;
+let request = new APIRequest();
+let apiPath: string = "";
+let getUserPage: string = "/?page=2";
+let body: any = null;
+
+export class userAPI {
+  public getUsers() {
+    let apiPath: string = `${getUserPage}`;
+
+    request.GET(apiPath, body).then((res: any) => {
+      console.log("retrieved: ", res);
+      expect(res.status).to.eq(200);
+    });
+    return this;
+  }
+
+  public createUser() {
+    request.POST(apiPath, user_payload).then((res: any) => {
+      console.log("retrieved: ", res);
+      expect(res.status).to.eq(200);
+    });
+    return this;
+  }
+}
